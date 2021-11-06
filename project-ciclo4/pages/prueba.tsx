edfedc309b26c93759a864e024db42567e6d2304
Johan Ussa Rubio <johanuss0405@gmail.com>
2021-11-06 10:32:25 -0500

    Eliminando algunos archivos basura

---
+++ project-ciclo4/pages/prueba.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const prueba = () => {
+    return (
+        <div>
+            <h5>Prueba de despliegue con routes</h5>
+        </div>
+    )
+}
+
+export default prueba
